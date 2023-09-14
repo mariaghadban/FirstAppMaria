62afa91ee24f4100ed2351ff290c5490cfc8ea3a
Nardeen230505 <nardeeno2005@gmail.com>
2023-09-14 14:59:29 +0300

    hh

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,13 +3,32 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //gdf
+        Log.d("", "");
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
+        protected void onRestart()
+        {
+            super.onRestart();
+            Log.d("","");
+            Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+        }
+        protected void onResum()
+        {
+        super.onResume();
+        Log.d()
+        }
+
+
+
 }
