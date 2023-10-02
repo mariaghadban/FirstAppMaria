d367662f8a70f011329b12db345b245e65b3a8aa
Nardeen230505 <nardeeno2005@gmail.com>
2023-10-02 13:58:56 +0300

    بعدين بكمل

--- app/src/main/java/com/example/firstapp/SignUpAtivity.java
+++ app/src/main/java/com/example/firstapp/SignUpAtivity.java
@@ -3,12 +3,33 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignUpAtivity extends AppCompatActivity {
+    private Button button2;
+    private TextInputEditText et_Email;
+    private TextInputEditText et_password;
+    private TextInputEditText et_repassword;
+    private TextInputEditText et_name;
+    private TextInputEditText et_phone;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_ativity);
+
+        button2=findViewById(R.id.button2);
+        et_Email=findViewById(R.id.et_Email);
+        et_password=findViewById(R.id.et_password);
+        et_repassword=findViewById(R.id.et_repassword);
+        et_name=findViewById(R.id.et_name);
+        et_phone=findViewById(R.id.et_phone);
+
+
+
     }
 }
--- app/src/main/java/com/example/firstapp/signinactivity.java
+++ app/src/main/java/com/example/firstapp/signinactivity.java
@@ -3,11 +3,15 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 public class signinactivity extends AppCompatActivity {
     private TextInputEditText etEmail;
+    private TextInputEditText etPassword;
+    private Button btn_signin;
+    private Button btn_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,10 @@ public class signinactivity extends AppCompatActivity {
         setContentView(R.layout.signinactivity);
 
         etEmail=findViewById(R.id.etEmail);
+        etPassword=findViewById(R.id.etPassword);
+        btn_signin=findViewById(R.id.btn_signin);
+        btn_signup=findViewById(R.id.btn_signup);
+
 
 
     }
