644e18e890caa23e6bebf9526d46ea9b39e8fa82
Nardeen230505 <nardeeno2005@gmail.com>
2023-10-30 14:12:10 +0200

    بعدين بكمل

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private SearchView srchVeiw;
+    private Spinner spnrSubject;
+    private
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/firstapp/add_task.java
+++ app/src/main/java/com/example/firstapp/add_task.java
@@ -1,5 +1,6 @@
 package com.example.firstapp;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -9,14 +10,13 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputLayout;
-
-public class edit_task_activity extends AppCompatActivity {
-    private Button updateBtn;
-    private Button cancelBtn;
-    private TextView tvImportance2;
-    private SeekBar seekBarUpdate;
-    private TextInputLayout etTextAd;
+public class add_task extends AppCompatActivity {
+    private Button saveBtnTask;
+    private Button cancelBtnTask;
+    private TextView tvImportance;
+    private SeekBar seekBarIm;
     private TextInputLayout titleShort;
+    private TextInputLayout etTextAdd;
 
 
     @SuppressLint("MissingInflatedId")
@@ -25,12 +25,12 @@ public class edit_task_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_task);
 
-        etTextAd=findViewById(R.id.etTextAd);
+        etTextAdd=findViewById(R.id.etTextAd);
         titleShort=findViewById(R.id.titleShort);
-        seekBarUpdate=findViewById(R.id.seekBarUpdate);
-        tvImportance2=findViewById(R.id.tvImportance2);
-        cancelBtn=findViewById(R.id.cancelBtn);
-        updateBtn=findViewById(R.id.updateBtn);
+        seekBarIm=findViewById(R.id.seekBarIm);
+        tvImportance=findViewById(R.id.tvImportance);
+        cancelBtnTask=findViewById(R.id.cancelBtnTask);
+        saveBtnTask=findViewById(R.id.saveBtnTask);
 
 
     }
